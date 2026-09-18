6d164e7e1d0e3524f8dc2a2c94119357fd1b4f08
sargentogato <marlonjvelasquez@gmail.com>
2026-09-18 16:37:44 +0200

    take off hasf of web adress

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: () => import("./pages/HomePage.vue") },
@@ -20,7 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return { top: 0 };
